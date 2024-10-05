d823993d7f118b7c0f4ecd58967ddc8a2277e07c
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-10-05 13:04:01 +0100

    get one service controller added

---
+++ controllers/services.js
@@ -0,0 +1,29 @@
+const service = require("../models/service")
+const statusCodes = require("http-status-codes")
+
+const createService = async (req,res)=> {
+        try {
+    const newService = await service.create(req.body)
+    return res.status(statusCodes.CREATED).json({newService})
+        }catch(error) {
+    return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({msg:error})
+        }
+}
+
+const getOneService = async (req,res)=> {
+    try{
+        const _service = await service.findOne({_id:req.params.id})
+        if(!_service) {
+            return res.status(statusCodes.NOT_FOUND).json({msg:`no service with id ${req.params.id} found`})
+        }
+        return res.status(statusCodes.OK).json({_service})
+    }catch(error) {
+        return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({msg:error})
+    }
+
+    
+}
+
+//return service
+
+//change price
