fb63cfe92055017ff178ce0c2a44b0522a512d83
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-10-11 21:02:37 +0100

    restructured calculate discount controller

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -3,44 +3,22 @@ const hospitalDiscount = require("../models/hospitalDiscount")
 const statusCodes = require("http-status-codes");
 
 const calculateDiscount = async (req, res) => {
+
     try {
-        const { referredFrom } = req.query;
+         const {hospitalName } = req.query
 
-        // Find patients with services referred from the specified source
-        const patientsReferredFrom = await patient.find({ "service.referredFrom": referredFrom });
+         hospital = await hospitalDiscount.findOne({Name:hospitalName})
+         if(!hospital) {
+            res.status(statusCodes.NOT_FOUND).json({msg:`No hospital with name ${hospitalName} found`})
+         }
 
-        if (patientsReferredFrom.length === 0) {
-            return res.status(statusCodes.NOT_FOUND).json({ msg: `No patients referred from ${referredFrom}` });
-        }
+         const totalDiscount = hospital.totalDiscount
 
-        // Initialize totalAmount
-        let totalAmount = 0;
-
-        // Iterate over each patient
-        for (let index = 0; index < patientsReferredFrom.length; index++) {
-            const patient = patientsReferredFrom[index];
-
-            // Iterate over each service
-            for (let j = 0; j < patient.service.length; j++) {
-                let service = patient.service[j];
-
-                // Sum only the services that are referred from "peaceful health"
-                if (service.referredFrom === referredFrom) {
-                    totalAmount += service.amountPaid;
-                }
-            }
-        }
-
-        // Calculate 10% discount
-        const discount = (10 / 100) * totalAmount;
-
-        res.status(statusCodes.OK).json({
-            msg: `The total discount for referrals from ${referredFrom} is ${discount}`,
-            totalAmount,
-        });
-    } catch (error) {
-        res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ msg: error.message });
+        res.status(statusCodes.OK).json({hospital,msg:` total discount is ${totalDiscount}`})
+    }catch(error) {
+        res.status(statusCodes.INTERNAL_SERVER_ERROR).json({msg:error})
     }
+    
 };
 
 const returnAllHospitalAndDiscount = async (req, res) => {
